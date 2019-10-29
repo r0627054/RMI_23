ce6e89d685ecfa78d3af158d7ec0f25768854154
Dries Janse <djse@live.be>
2019-10-29 12:44:22 +0100

    report update

--- src/server/ServerApplication.java
+++ src/server/ServerApplication.java
@@ -11,6 +11,7 @@ public class ServerApplication {
 	public static void main(String[] args) throws NumberFormatException, ReservationException, IOException, NotBoundException {
 		//Start all the server applications
 		String[] emptyArguments = {};
+		//System.setProperty("java.rmi.server.hostname","192.168.99.1");
 		NamingServiceApplication.main(emptyArguments);
 		RentalAgencyApplication.main(emptyArguments);
 		DockxRentalApplication.main(emptyArguments);
